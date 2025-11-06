c7281618240d638ecf6594a9b96b81a00aa0e968
Raphaël Bedleem <rbedleem@myges.fr>
2025-11-06 14:19:46 +0100

    fix: rewrite javascript for clean code

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-document.getElementById('addTaskButton').addEventListener('click', function() {
-            const taskInput = document.getElementById('taskInput');
-            const taskText = taskInput.value.trim();
-            if (taskText !== '') {
-                const taskList = document.getElementById('taskList');
-                const listItem = document.createElement('li');
-                listItem.textContent = taskText;
-                taskList.appendChild(listItem);
-                taskInput.value = '';
-            }
-        });
+const addTaskButton = document.querySelector('#addTaskButton');
+const taskInput = document.querySelector('#taskInput');
+
+addTaskButton.addEventListener('click', addTask);
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        const taskList = document.querySelector('#taskList');
+        const listItem = document.createElement('li');
+        listItem.textContent = taskText;
+        taskList.appendChild(listItem);
+        taskInput.value = '';
+    }
+}
